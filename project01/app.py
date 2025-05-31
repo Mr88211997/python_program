b14f13d4a20c9a09f026a883571c1724ee5d646d
mr88211997 <yaksh@hdit.com>
2025-06-01 02:27:33 +0530

    update

--- project01/app.py
+++ project01/app.py
@@ -13,13 +13,18 @@ def register():
     return render_template('register.html')
 @app.route('/submit', methods = ['GET'])
 def submit():
+    err = "email id and password is not valid! "
     email = request.args.get('email')
     pwd = request.args.get('password')
     # msg = f"you have enter the username : {username} \n password : {password}"
     # return msg
     if email == 'demo@hdit.com':
         if pwd == '12345':
-            return redirect(url_for('deshboard'))
+            return render_template('deshboard.html') #if you want to use jinja template  redirect(url_for('deskboard'))
+        else:
+            return render_template('login.hmtl', err=err ) # if you want to use jinja template redirect(url_for('login'))
+    else:
+        return render_template('login.html', err=err ) 
 
 @app.route('/deshboard')
 def deshboard():
